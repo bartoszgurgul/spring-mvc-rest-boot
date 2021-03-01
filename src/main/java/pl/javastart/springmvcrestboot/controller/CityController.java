ffb1ba76e59e8e8f4362cdfdd9aaf011e99e7854
bartoszgurgul <bartoszgurgul@gmail.com>
2021-03-01 22:44:55 +0100

    lekcja obsluga zadan przychodzacych czyli tworzenie aplikacji ktora dodaje cale okiekty przy pomocy RequestBody parametru

--- src/main/java/pl/javastart/springmvcrestboot/controller/CityController.java
+++ src/main/java/pl/javastart/springmvcrestboot/controller/CityController.java
@@ -1,13 +1,46 @@
 package pl.javastart.springmvcrestboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import pl.javastart.springmvcrestboot.model.City;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 @RestController
+@RequestMapping("/cities")
 public class CityController {
-    @RequestMapping("/city")
-    public City getCity(){
-        return new City("Warsawa", 1_700_000);
+
+    List<City> cities;
+
+    public CityController(){
+        cities = Collections.synchronizedList(new ArrayList<>());
+    }
+
+
+    @GetMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<City> getCities(@RequestParam(defaultValue = "name") String orderBy){
+        List<City> citiesCopy = new ArrayList<>(cities);
+        if ("name".equals(orderBy)){
+            citiesCopy.sort(Comparator.comparing(City::getName));
+        } else if ("population".equals(orderBy)){
+            citiesCopy.sort(Comparator.comparing(City::getPopulation));
+        }
+        return citiesCopy;
+    }
+
+    @GetMapping("{id}")
+    public City getCity(@PathVariable int id){
+        return cities.get(id);
+    }
+
+    @PostMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void save(@RequestBody City city){
+        cities.add(city);
     }
 }
